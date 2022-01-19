08e51dc4889984fd2cedee89572d8af51e07f2ee
Syahrul yuda <90370914+syahrulyudaa@users.noreply.github.com>
2022-01-19 21:27:40 +0700

    Add files via upload

---
+++ TugasAkhir/alert.js
@@ -0,0 +1,7 @@
+var n = prompt("Masukan nama anda:");
+var c = confirm("Hai " + n + "! Udah Makan belom?");
+if (c == true) {
+    alert("jaga kesehatan ya!!");
+} else {
+    alert("makan dulu gih");
+}
